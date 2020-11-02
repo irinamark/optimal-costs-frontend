004838e5c700c68fa3219db64b2a2b807727953c
Irina M <iramark@leansdk.net>
2020-11-02 20:52:24 +0300

    add registration new users

--- app/controllers/home/index.js
+++ app/controllers/home/index.js
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 
 export default class HomeIndexController extends Controller {
   form = {
@@ -13,6 +14,29 @@ export default class HomeIndexController extends Controller {
     const user = (await axios.get(`http://localhost:5000/users/${this.form.name}`)).data;
     if (user.password == this.form.password) {
       this.transitionToRoute('home.user', user.id);
+    }
+  }
+
+  @action
+  async registration() {
+    const userId = uuidv4();
+    const user = await axios.post(`http://localhost:5000/users`, {
+      id: userId,
+      name: this.form.name,
+      password: this.form.password
+    });
+    if(user.status == 201) {
+      const projectId = uuidv4()
+      const project = await axios.post(`http://localhost:5000/projects`, {
+        id: projectId,
+        balance: 0,
+        title: `Project-${projectId} `,
+        date: new Date().toISOString(),
+        userId: userId
+      });
+      if(project.status == 201) {
+        alert('User created!')
+      }
     }
   }
 }
